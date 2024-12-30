33e48857b47b7c759f3885e8213ae12a4ad906cf
0xheg3zy <163908953+0xheg3zy@users.noreply.github.com>
2024-12-30 20:20:02 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,8 +10,7 @@ vncserver -geometry 1600x900 -depth 16 :1
 git clone https://github.com/novnc/noVNC /root/noVNC
 cp ~/noVNC/vnc.html ~/noVNC/index.html
 cp /self.pem ~/noVNC/
-x11vnc -display :1 -autoport -localhost -nopw -bg -xkb -ncache -ncache_cr 
--quiet -forever
+x11vnc -display :1 -autoport -localhost -nopw -bg -xkb -ncache -ncache_cr -quiet -forever
 ~/noVNC/utils/novnc_proxy --vnc localhost:5901 --listen 80 &
 clear
 startxfce4
